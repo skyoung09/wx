cced32a7b822f57c028a2d26328d881280a1a03e
zhangxiaobo02 <zhangxiaobo02@baidu.com>
2018-06-10 07:20:57 +0800

    Init

---
+++ app/src/main/java/com/imooc/weixin/ChatMainTabFragment.java
@@ -0,0 +1,19 @@
+package com.imooc.weixin;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import imooc.com.weixin.R;
+
+public class ChatMainTabFragment extends Fragment
+{
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState)
+	{
+		return inflater.inflate(R.layout.tab01, container, false);
+	}
+}
---
+++ app/src/main/java/com/imooc/weixin/ContactMainTabFragment.java
@@ -0,0 +1,19 @@
+package com.imooc.weixin;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import imooc.com.weixin.R;
+
+public class ContactMainTabFragment extends Fragment
+{
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState)
+	{
+		return inflater.inflate(R.layout.tab03, container, false);
+	}
+}
